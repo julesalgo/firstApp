5d3fdb11a142508a9b698aac7e8588e9918f724c
jules <j.bonard@algotech.fr>
2020-02-12 09:09:31 +0100

    Angular 02, le binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 @NgModule({
-  declarations: [AppComponent, SignUpComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, SignUpComponent, UserProfileComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
